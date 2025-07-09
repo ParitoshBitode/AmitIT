5052d321805a167db57c918b4465103b0fcd3494
Paritosh <paritosh.bitode@gmail.com>
2025-07-09 20:33:42 +0530

    Second Demo Project added

---
+++ AmitIT/src/Demo1.java
@@ -0,0 +1,16 @@
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a = 10;
+		int b = 20;
+		
+		int c = a + b;
+		
+		System.out.println("Addition of a + b is "+ c);
+	}
+
+}
